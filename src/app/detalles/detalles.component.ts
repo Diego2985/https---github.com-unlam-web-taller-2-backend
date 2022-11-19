6eec8b1122ff774942b7ad2c14e0033e256e2c21
Diego2985 <diego85xbox@gmail.com>
2022-11-18 23:11:47 -0300

    Se sube frontend con login y detalles

---
+++ src/app/detalles/detalles.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles',
+  templateUrl: './detalles.component.html',
+  styleUrls: ['./detalles.component.css']
+})
+export class DetallesComponent {
+// public detalle: [] = [
+ {
+   "id": 1,
+   "title": 
+   "price":
+   "description":
+   "category":
+   "image":
+   "rate":
+   "stock":  
+ },
+ {
+  "id": 2,
+  "title": 
+  "price":
+  "description":
+  "category":
+  "image":
+  "rate":
+  "stock":  
+},
+{
+  "id": 3,
+  "title": 
+  "price":
+  "description":
+  "category":
+  "image":
+  "rate":
+  "stock":  
+},
+{
+  "id": 4,
+  "title": 
+  "price":
+  "description":
+  "category":
+  "image":
+  "rate":
+  "stock":  
+},
+ ]
+  
+}
